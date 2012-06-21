bf758dcf25e38078f310f93fa8b73a77162a01cf
Arley Lobato <arleylobato@gmail.com>
2012-06-20 23:02:20 -0300

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,7 +65,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+      sudo "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
 
@@ -81,7 +81,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+    # run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
